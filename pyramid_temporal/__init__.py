a2eada669141918f53d46ce6ad86f42ad367a4f2
“Tomas <tomas.correa@gmail.com>
2026-09-23 18:11:00 +0000

    bump: v0.1.3

--- pyramid_temporal/__init__.py
+++ pyramid_temporal/__init__.py
@@ -52,7 +52,7 @@ if TYPE_CHECKING:
     from pyramid.config import Configurator
     from pyramid.request import Request
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 # Main public API
 # Create an 'activity' module-like namespace for @activity.defn syntax
